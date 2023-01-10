7c89f61b27db0a2d77baf257c3e865e4eae34007
RodentCoder <ichen080405@gmail.com>
2023-01-10 13:03:00 -0500

    api

--- API project/JS/main.js
+++ API project/JS/main.js
@@ -1,1 +1,9 @@
 import "../Styles/styles.css";
+
+const api = "https://xkcd.com/614/info.0.json";
+
+async function getData(api) {
+  const response = await fetch(api);
+  console.log(response);
+}
+getData(api);
