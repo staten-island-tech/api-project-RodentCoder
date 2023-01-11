02c5a8b330efa7699b666b1ff60817de0a6cae95
RodentCoder <ichen080405@gmail.com>
2023-01-11 13:17:32 -0500

    CORS POLICY

--- API project/JS/main.js
+++ API project/JS/main.js
@@ -1,9 +1,8 @@
-import "../Styles/styles.css";
-
 const api = "https://xkcd.com/614/info.0.json";
 
-async function getData(api) {
+async function getData() {
   const response = await fetch(api);
   console.log(response);
 }
+
 getData(api);
