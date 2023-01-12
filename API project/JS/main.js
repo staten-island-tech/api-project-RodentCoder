55b9321132554f04f77150d4f6c29d4edc063b5e
RodentCoder <ichen080405@gmail.com>
2023-01-12 13:15:04 -0500

    Welp

--- API project/JS/main.js
+++ API project/JS/main.js
@@ -1,8 +1,13 @@
-const api = "https://xkcd.com/614/info.0.json";
+const api = "https://pokeapi.co/api/v2/pokemon/ditto";
 
-async function getData() {
-  const response = await fetch(api);
-  console.log(response);
+async function getData(api) {
+  try {
+    const response = await fetch(api);
+    const data = await response.json();
+    console.log(data.content);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 getData(api);
